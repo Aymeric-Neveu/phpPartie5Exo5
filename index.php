80794444ef5dbef025e86b8d23a2d13b601950b5
root <emploi.neveu@gmail.com>
2018-11-23 10:49:05 +0100

    exo5

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+$département = array (
+    69 => 'Nord-Pas-Calais',
+    80 => 'Somme',
+    02 => 'Aisne',
+    59 => 'Nord',
+    60 => 'Oise');
+    ?>
+  </body>
+</html>
